d39c050437e63a02ac66e8046e241b8c7d1ed099
heyuantao <he_yuan_tao@163.com>
2020-08-13 23:32:09 +0800

    continue

--- src/main/java/cn/heyuantao/onlinejudgeserver/controller/OnlineJudgeClientController.java
+++ src/main/java/cn/heyuantao/onlinejudgeserver/controller/OnlineJudgeClientController.java
@@ -1,23 +1,71 @@
 package cn.heyuantao.onlinejudgeserver.controller;
 
+import cn.heyuantao.onlinejudgeserver.service.OnlineJudgeClientService;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @author he_yu
  * 处理来自判题机的各种请求
  */
+@Api(tags={"判题机接口"})
 @Controller
-@RequestMapping("/api/onlinejudgeclient/")
+@RequestMapping("/api/v1/onlinejudgeclient/")
 public class OnlineJudgeClientController {
 
-    @GetMapping
-    public ResponseEntity<String> handleClientRequest(){
-        return new ResponseEntity("hello", HttpStatus.ACCEPTED);
+    @Autowired
+    OnlineJudgeClientService onlineJudgeClientService;
+
+    @PostMapping("/getjobs/")
+    public ResponseEntity<String> getJobs(){
+        List<String> jobs = onlineJudgeClientService.getJobs();
+        StringBuilder stringBuilder = new StringBuilder();
+        for(String oneLine:jobs){
+            stringBuilder.append(oneLine);
+            stringBuilder.append("\n");
+        }
+        return new ResponseEntity(stringBuilder.toString(),HttpStatus.ACCEPTED);
     }
 
+
+    @PostMapping("/updatesolution/")
+    public ResponseEntity<String> updateSolution(
+            @RequestParam(value="sid") String sid,
+            @RequestParam(value="result") String result,
+            @RequestParam(value="time") String time,
+            @RequestParam(value="memory") String memory,
+            @RequestParam(value="sim") String sim,
+            @RequestParam(value="sim_id") String sim_id,
+            @RequestParam(value="pass_rate") String pass_rate
+    ){
+        onlineJudgeClientService.updateSolution(sid,result,time,memory,sim,sim_id,pass_rate);
+        return new ResponseEntity("",HttpStatus.ACCEPTED);
+    }
+
+
+    @PostMapping("/addcompileerrorinformation/")
+    public ResponseEntity<String> addCompileErrorInformation(
+            @RequestParam(value="sid") String sid,
+            @RequestParam(value="ceinfo") String ceinfo
+    ){
+        onlineJudgeClientService.addCompileErrorInformation(sid,ceinfo);
+        return new ResponseEntity("",HttpStatus.ACCEPTED);
+    }
+
+
+    @PostMapping("/getsolution/")
+    public ResponseEntity<String> getSolution(
+            @RequestParam(value="sid") String sid
+    ){
+        String solution_content = onlineJudgeClientService.getSolution(sid);
+        return new ResponseEntity(solution_content,HttpStatus.ACCEPTED);
+    }
 
 }
--- src/main/java/cn/heyuantao/onlinejudgeserver/service/OnlineJudgeClientService.java
+++ src/main/java/cn/heyuantao/onlinejudgeserver/service/OnlineJudgeClientService.java
@@ -1,8 +1,39 @@
 package cn.heyuantao.onlinejudgeserver.service;
 
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author he_yu
  * 将用户的数据存储在Redis中
  */
+@Service
 public class OnlineJudgeClientService {
+
+    public List<String> getJobs() {
+        List<String> list=new ArrayList<>();
+        list.add("121212");
+        list.add("2223232");
+        return list;
+    }
+
+    public void updateSolution(String sid, String result, String time, String memory, String sim, String sim_id, String pass_rate) {
+
+    }
+
+    public void addCompileErrorInformation(String sid, String ceinfo) {
+
+    }
+
+
+    /**
+     * 返回某个Soludion的程序代码
+     * @param sid
+     * @return
+     */
+    public String getSolution(String sid) {
+        return "";
+    }
 }
