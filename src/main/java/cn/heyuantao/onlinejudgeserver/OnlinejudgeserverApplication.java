2a5227f50b9c72c8561e9e85ee48976a2d8649f7
heyuantao <he_yuan_tao@163.com>
2020-08-13 14:43:11 +0800

    first

---
+++ src/main/java/cn/heyuantao/onlinejudgeserver/OnlinejudgeserverApplication.java
@@ -0,0 +1,13 @@
+package cn.heyuantao.onlinejudgeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinejudgeserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlinejudgeserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/heyuantao/onlinejudgeserver/OnlinejudgeserverApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.heyuantao.onlinejudgeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinejudgeserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
