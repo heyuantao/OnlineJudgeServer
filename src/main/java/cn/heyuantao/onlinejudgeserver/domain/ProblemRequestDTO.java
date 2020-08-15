fb11ff1bdc53bbc27cd115d36c45b8bf229e1186
heyuantao <he_yuan_tao@163.com>
2020-08-15 14:56:44 +0800

    work

--- src/main/java/cn/heyuantao/onlinejudgeserver/domain/ProblemRequestDTO.java
+++ src/main/java/cn/heyuantao/onlinejudgeserver/domain/ProblemRequestDTO.java
@@ -4,6 +4,7 @@ package cn.heyuantao.onlinejudgeserver.domain;
 import cn.heyuantao.onlinejudgeserver.core.Problem;
 import cn.heyuantao.onlinejudgeserver.core.ProblemTestCase;
 import cn.heyuantao.onlinejudgeserver.validator.AllowString;
+import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -15,26 +16,46 @@ import java.util.List;
  * 第三方客户端提交的数据
  */
 public class ProblemRequestDTO implements Serializable {
+    /**
+     * 用户提交的代码
+     */
     @NotEmpty(message = "源代码不能为空")
+    @Length(min=1, max=1024, message = "代码长度不能为0且不能超过1024字节")
     private String sourceCode;
 
+    /**
+     * 语言的类型，用扩展名进行标识
+     */
     @NotEmpty(message = "扩展名不能为空")
     @AllowString(message = "所填写的扩展名系统不支持",allows = {"C","CPP","JAVA"})
     private String langExtension;
 
+    /**
+     * 默认时间限制默认为1秒
+     */
     @NotNull(message = "时间限制不能为空")
-    private Integer timeLimit;
+    private Integer timeLimit=1;
 
+    /**
+     * 默认内存限制为128M
+     */
     @NotNull(message = "内存限制不能为空")
-    private Integer memoryLimit;
+    private Integer memoryLimit=128;
 
+    /**
+     * 是否需要特殊判定,默认为False
+     */
     private Boolean isSpecialJudge=Boolean.FALSE;
 
+    /**
+     * 针对代码的测试用例，至少有一个测试用例
+     */
     @NotNull(message = "测试用例不能为空")
     private List<ProblemTestCase> testCaseList;
 
     @NotEmpty(message = "回调地址不能为空")
     private String notifyAddress;
+
 
     public ProblemRequestDTO(String sourceCode, String langExtension, Integer timeLimit, Integer memoryLimit, Boolean isSpecialJudge, List<ProblemTestCase> testCaseList, String notifyAddress) {
         this.sourceCode = sourceCode;
