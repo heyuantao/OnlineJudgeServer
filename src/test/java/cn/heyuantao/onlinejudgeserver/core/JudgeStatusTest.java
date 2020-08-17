95c8e2cfeb9bbc7daaf3a4dabcae4bdf328f5681
heyuantao <he_yuan_tao@163.com>
2020-08-17 22:26:02 +0800

    continue

--- src/test/java/cn/heyuantao/onlinejudgeserver/core/JudgeStatusTest.java
+++ src/test/java/cn/heyuantao/onlinejudgeserver/core/JudgeStatusTest.java
@@ -1,9 +1,13 @@
 package cn.heyuantao.onlinejudgeserver.core;
 
+import org.apache.commons.lang3.EnumUtils;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -24,7 +28,34 @@ class JudgeStatusTest {
         judgeStatus = JudgeStatus.getJudgeStatusByValue(12);
         System.out.println(judgeStatus);
 
-        judgeStatus = JudgeStatus.getJudgeStatusByValue(20);
+/*        judgeStatus = JudgeStatus.getJudgeStatusByValue(20);
+        System.out.println(judgeStatus);*/
+
+        List<JudgeStatus> judgeStatusList = EnumUtils.getEnumList(JudgeStatus.class);
+        System.out.println(judgeStatusList);
+    }
+
+    @Test
+    void isInPendingStatus() {
+        JudgeStatus judgeStatus = null;
+        Boolean status = null;
+
+        judgeStatus = JudgeStatus.AC;
         System.out.println(judgeStatus);
+        System.out.println(judgeStatus.getName());
+        System.out.println(judgeStatus.getValue());
+        status = judgeStatus.isInPendingStatus();
+        Assert.assertEquals(status,Boolean.FALSE);
+        System.out.println(status);
+
+
+        judgeStatus = JudgeStatus.PD;
+        System.out.println(judgeStatus);
+        System.out.println(judgeStatus.getName());
+        System.out.println(judgeStatus.getValue());
+        status = judgeStatus.isInPendingStatus();
+        Assert.assertEquals(status,Boolean.TRUE);
+        System.out.println(status);
+
     }
 }
